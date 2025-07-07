1fec7740fa6d7713b87da9c826a11bac5806c929
RudraP-11285 <146158907+RudraP-11285@users.noreply.github.com>
2025-07-07 15:05:25 -0700

    Create MotorCoupling.java

---
+++ MotorCoupling.java
@@ -0,0 +1,15 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class MotorCoupling {
+    // Ordered map of motor -> isReversed
+    public Map<DcMotor, Boolean> motors = new LinkedHashMap<>();
+
+    public void addMotor(DcMotor motor, boolean reversed) {
+        motors.put(motor, reversed);
+    }
+}
